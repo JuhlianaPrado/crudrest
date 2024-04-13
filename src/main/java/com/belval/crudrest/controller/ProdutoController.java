d138c50250ceeee64800c460b0f6f6c7d35cdf79
Juliana Nunes do Prado <juhliananunes@icloud.com>
2024-04-12 22:46:43 -0300

    Add método apagarProduto

--- src/main/java/com/belval/crudrest/controller/ProdutoController.java
+++ src/main/java/com/belval/crudrest/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,7 +64,12 @@ public class ProdutoController {
 				.body(produto);
 	}
 	
+	@DeleteMapping("/produtos/{id}")
+    public ResponseEntity<Object> apagarProduto(
+    		@PathVariable(value = "id") Integer id){
+	     return null;
+	
+      }
 
-	
-	
 }
+
